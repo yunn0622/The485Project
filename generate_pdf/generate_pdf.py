74391e1794eb07703bf9df030c8b97ce7c1b2aee
yun <yunn0622@github.com>
2022-09-16 16:34:18 -0700

    combined generate_pdf with retrieve_from_db to get the data from DynamoDB in one function

--- generate_pdf/generate_pdf.py
+++ generate_pdf/generate_pdf.py
@@ -1,10 +1,19 @@
 import boto3
 from PyPDF2 import PdfReader, PdfWriter
 
+dynamodb = boto3.client('dynamodb')
 s3_client = boto3.client("s3")
 S3_BUCKET_NAME = '485project'
 object_key = "i-485_first_page.pdf"
 
+def retrieve_from_db(key):
+    data = dynamodb.get_item(
+        TableName='485_test',
+        Key={
+            'ID': {"S": key}
+        })
+    value = data["Item"]["Name"]["S"]
+    return value
 
 def lambda_handler(event, context):
     writer = PdfWriter()
@@ -16,16 +25,15 @@ def lambda_handler(event, context):
     writer.add_page(page)
     writer.update_page_form_field_values(
         writer.pages[0], {
-            'Pt1Line1a_FamilyName[0]': 'this is',
-            'Pt1Line1b_GivenName[0]': 'test',
-            'Pt1Line2a_FamilyName[0]': 'Hello'
+            'Pt1Line1a_FamilyName[0]': retrieve_from_db('Pt1Line1a_FamilyName[0]'),
+            'Pt1Line1b_GivenName[0]': retrieve_from_db('Pt1Line1b_GivenName[0]'),
+            'Pt1Line2a_FamilyName[0]': retrieve_from_db('Pt1Line2a_FamilyName[0]')
         }
     )
     output_file = '/tmp/filled_out.pdf'
 
     with open(output_file, 'wb') as output_stream:
         writer.write(output_stream)
+    s3_client.upload_file(output_file, S3_BUCKET_NAME, 'filled_out.pdf')
 
-    response = s3_client.upload_file(output_file, S3_BUCKET_NAME, 'filled_out.pdf')
-
-    return response
+    return {'message': 'Filled out pdf file has been successfully saved to the s3 bucket'}
