a7f03d2dfec3ddd21cc13ef19b1398838688a25e
yun <yunn0622@github.com>
2022-11-02 14:02:43 -0700

    changed put_item to batch.put_item so multiple data can be saved at once

--- save_to_db/save_to_db.py
+++ save_to_db/save_to_db.py
@@ -4,17 +4,26 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('485_test')
 
+
 def lambda_handler(event, context):
-	ID = event['key']
-	name = event['value']
+    last_name = event['key']
+    first_name = event['value']
 
-	table.put_item(
-		Item={
-			'ID': ID,
-			'Name': name
-		})
+    with table.batch_writer() as batch:
+        batch.put_item(
+            Item={
+                'ID': "Pt1Line1a_FamilyName[0]",
+                'Name': last_name
+            }
+        )
+        batch.put_item(
+            Item={
+                'ID': "Pt1Line1b_GivenName[0]",
+                'Name': first_name
+            }
+        )
 
-	return {
-		'statusCode': 200,
-		'body': json.dumps('Successfully save to database')
-	}
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successfully save to database')
+    }
