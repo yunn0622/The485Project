bef4edaf05b7580d0e67cb9d94b0a84a017dcf0c
yun <yunn0622@github.com>
2022-12-01 12:37:33 -0800

    config.js for cognito

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_xHZNxcKEg',
+        userPoolClientId: '7k5fgtj25mh8l2pkjtj71btm3d',
+        region: 'us-west-2'
+    },
+    api: {
+        invokeUrl: 'https://7ctegn7d3j.execute-api.us-west-2.amazonaws.com/dev'
+    }
+};
