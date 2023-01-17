47749b01399cb6a6a460cf7dd355c3bca41640a4
yun <yunn0622@github.com>
2023-01-17 14:58:38 -0800

    fixed the issue with name component

--- react-485/src/App.js
+++ react-485/src/App.js
@@ -4,6 +4,7 @@ import { Flex, Button, ButtonGroup, View, TextField } from '@aws-amplify/ui-reac
 import InputBox from './InputBox';
 import { fields } from './fields';
 import '@aws-amplify/ui-react/styles.css';
+import Form from './Form';
 // import axios from 'axios';
 
 
@@ -12,7 +13,6 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      key: ''
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,17 +20,19 @@ class App extends Component {
 
 
   async handleChange(event) {
-    this.setState({lname: event.target.value});
     this.setState({key: event.target.name});
+    this.setState({value: event.target.value})
+    const inputArr = []
+    
   }
 
   async handleSubmit(event) {
     event.preventDefault();
     console.log("this.state: ", this.state)
-    const inputValue = this.state.lname;
-    const keyy = this.state.key;
-    console.log("key:", keyy, "inputval: ", inputValue)
-    const raw = JSON.stringify({"key": keyy, "value": inputValue})
+    const key = this.state.key;
+    const value = this.state.value;
+    console.log("key:", key, "inputval: ", value)
+    const raw = JSON.stringify({"key": key, "value": value})
     try{
       fetch("https://7ctegn7d3j.execute-api.us-west-2.amazonaws.com/dev", {method: 'POST', body: raw})
       .then(response => response.text())
@@ -42,23 +44,30 @@ class App extends Component {
   }
 
   render () {
+    const fieldsArray = fields.map((field, i) => {
+      return (
+          <InputBox 
+              key={i}
+              label={fields[i].label}
+              name={fields[i].name}
+              onChange={this.handleChange}
+              />
+      );
+    })
     return( 
       <View as="form" onSubmit={this.handleSubmit}>
         <Flex direction="column" padding="2rem" alignItems="center">
-          <InputBox 
+          {/* <InputBox 
             label={fields[0].label}
-            name={fields[0].attr}
+            name={fields[0].name}
             onChange={this.handleChange}
             />
-          {/* <TextField 
-            label={fields[0].label}
-            attr={fields[0].attr}
-            errorMessage="There is an error"
-            // onChange={this.handleChange}
-            onChange={event => console.log(event.target.value)}
-
-          /> */}
-          {/* <InputBox label={fields[1].label}/> */}
+          <InputBox 
+            label={fields[1].label}
+            name={fields[1].name}
+            onChange={this.handleChange}
+            /> */}
+            { fieldsArray }
         <View direction="row">
           <ButtonGroup>
             <Button
